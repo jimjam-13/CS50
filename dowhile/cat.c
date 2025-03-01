cd91d1d349fdaab5750fdc3fe36a6abc33de10a7
Jithin Jimmy <jithin.akash.varun@gmail.com>
2025-03-01 17:37:08 +0530

    asking for positive int function with return value

--- dowhile/cat.c
+++ dowhile/cat.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
+int ask_positive_int(void);
 void meow(int times);
 
 int main(void) {
+  int times = ask_positive_int();
+  meow(times);
+}
+
+int ask_positive_int(void) {
   int times;
-  
+
   do {
     printf("No. of pets: ");
     scanf("%i", &times);
-  } while (times < 1);
-  
-  meow(times);
+  } while (times < 1);  // keep asking if the input is not positive
+
+  return times;  // return the value to the caller
 }
 
 void meow(int times) {
