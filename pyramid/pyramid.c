2715f8fce304473d511e96d3a730b9d8af8f8994
Jithin Jimmy <jithin.akash.varun@gmail.com>
2025-03-02 12:09:46 +0530

    right leaning pyramid

--- pyramid/pyramid.c
+++ pyramid/pyramid.c
@@ -8,6 +8,9 @@ void main(void) {
   scanf("%i", &n);
 
   for (int i = 0; i < n; i++) {
+    for (int k = n-1; k > i; k--) {
+      printf(" ");
+    }
     for (int j = 0; j <= i; j++) {
       printf("#");
     }
